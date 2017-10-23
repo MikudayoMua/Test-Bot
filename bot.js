ac8b987fffb795046d7c0e2b5c65ad57419f92d7
MikudayoMua <33037572+MikudayoMua@users.noreply.github.com>
2017-10-24 04:22:58 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const music = require('discord.js-music');
-const YoutubeDL = require('youtube-dl');
-const Request = require('request');
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -12,14 +9,6 @@ client.on('message', message => {
     if (message.content === 'รักหยวน') {
     	message.reply('ผมก็รักคุณ');
   	}
-});
-
-client.on('message', function() {
-    if (message.content === "$loop") { 
-      var interval = setInterval (function () {
-        message.channel.send("มาคุยกับ @mikudayo! Mua หรือชวนพี่เค้าเล่นเกมได้นะคับพี่เค้าขี้เหงา ^^")
-      }, 1 * 1000); 
-    }
 });
 
 client.login(process.env.BOT_TOKEN);
