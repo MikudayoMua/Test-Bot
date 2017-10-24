52af1d33867375bbe66e2aefa4d6c5ec5076b898
MikudayoMua <33037572+MikudayoMua@users.noreply.github.com>
2017-10-24 21:50:02 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,16 +11,4 @@ client.on('message', message => {
   	}
 });
 
-client.on('ready', function() {
-    console.log(bot.user.username);
-});
-
-client.on('message', function() {
-    if (message.content === "$loop") { 
-      const interval = setInterval (function () {
-        message.channel.send("มาคุยกับ@Mikudayo! Muaหรือชวนพี่เข้าเล่นเกมได้นะคับพี่เขาขี้เหงา")
-      }, 1 * 1000); 
-    }
-});
-
 client.login(process.env.BOT_TOKEN);
